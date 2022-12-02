a211b9b12e4cb8e247d1ee78a4942c4cf6b70542
Oliver Leissoo <oliverleissoo@gmail.com>
2022-12-02 17:35:08 +0200

    Hello world

---
+++ Hello World program.py
@@ -0,0 +1,9 @@
+"""Hello world program."""
+
+def main():
+    name = input("What is your name? ")
+    print("Hello, " + name + "!")
+"""Ask user's name and greet them."""
+
+if __name__ == '__main__':
+    main()
