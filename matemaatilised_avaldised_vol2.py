dca44df0627689f4b9fb6c85546352122e0d3b84
Oliver Leissoo <oliverleissoo@gmail.com>
2022-12-09 17:32:43 +0200

    lisatud kodutöö matemaatilised avaldised vol2

--- matemaatilised_avaldised_vol2.py
+++ matemaatilised_avaldised_vol2.py
@@ -1,24 +1,29 @@
+"""Math exercises vol2."""
+import math
 
 def time_converter(seconds: int) -> str:
     """Convert time in seconds to hours and minutes."""
     hours = seconds // 3600
-    minutes = seconds % 60
+    minutes = (seconds // 60) % 60
     return f"{seconds} sekundit on {hours} tund(i) ja {minutes} minut(it)."
-print(time_converter(3000))
+
 
 def student_helper(angle: int) -> str:
     """Return the sine and cosine of the given angle in degrees."""
-    # Write your code here
+    sine = round((math.sin(math.radians(angle))),1)
+    cosine = round((math.cos(math.radians(angle))),1)
     return f"Nurk: {angle}, siinus: {sine}, koosinus: {cosine}."
 
 
 def greetings(n: int) -> str:
     """Return a string that contains "Hey" n times."""
-    # Write your code here
+    lots_of_heys = n * "Hey"
     return lots_of_heys
 
 
 def adding_numbers(num_a: int, num_b: int) -> str:
     """Return given numbers added together as a string."""
-    # Write your code here
+    str_a = str(num_a)
+    str_b = str(num_b)
+    string_numbers = str_a + str_b
     return string_numbers
